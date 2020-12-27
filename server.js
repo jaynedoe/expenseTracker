46a3420b1e502e83dae960359eca11f7091263a0
jaynedoe <jaedoyne@hotmail.com>
2020-12-27 17:01:09 +1100

    finished metrics page, updated server

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
 const app = express();
 
 let expenseItems = [];
@@ -12,35 +11,54 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-
-    res.render("home", { items: expenseItems });
- 
+  res.render("home", { items: expenseItems });
 });
 
 app.post("/", function (req, res) {
-  
   let itemTitle = req.body.title;
   let itemCategory = req.body.category;
   let itemAmount = req.body.amount;
-  let date = new Date();
-  let hour = date.getHours();
-  let minutes = date.getMinutes;
 
   let expenseObject = {
-      title: itemTitle,
-      category: itemCategory,
-      amount: itemAmount
+    title: itemTitle,
+    category: itemCategory,
+    amount: itemAmount,
   };
 
-  console.log(expenseObject);
-  
   expenseItems.push(expenseObject);
 
-  res.redirect('/');  
+  res.redirect("/");
 });
 
-app.get('/dashboard', function(req, res){
-    res.render('dashboard');
+app.get("/dashboard", function (req, res) {
+  let totalDailySpend = 0;
+  let totalGrocerySpend = 0;
+  let totalFunSpend = 0;
+  let totalBillsSpend = 0;
+
+  expenseItems.forEach(function (item) {
+    totalDailySpend = Number(item.amount) + totalDailySpend;
+  });
+
+  expenseItems.forEach(function (item) {
+    if (item.category === "Groceries") {
+      totalGrocerySpend = Number(item.amount) + totalGrocerySpend;
+    } else if (item.category === "Fun") {
+      totalFunSpend = Number(item.amount) + totalFunSpend;
+    } else if (item.category === "Bills") {
+      totalBillsSpend = Number(item.amount) + totalBillsSpend;
+    }
+  });
+
+  let yearlySpend = totalDailySpend * 365;
+
+  res.render("dashboard", {
+    totalDailySpend: totalDailySpend,
+    yearlySpend: yearlySpend,
+    totalGrocerySpend: totalGrocerySpend,
+    totalBillsSpend: totalBillsSpend,
+    totalFunSpend: totalFunSpend,
+  });
 });
 
 app.listen(3000, function () {
